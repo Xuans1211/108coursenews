b6216fa4bd2af5411372cc3129ecfa1e15d7edeb
Lexi900 <lixinqin900@gmail.com>
2022-06-13 23:30:02 +0800

    製作部分（lexi）

--- export_code.R
+++ export_code.R
@@ -2,3 +2,19 @@ png("chart/g3.png", #請自己打檔案名稱，"chart/"這段不要改
     width=1280,height=900,units="px") #大小及單位，這裡有需要再調
 g3#要輸出的那個圖，原本在你Rstudio裡叫什麼
 dev.off()
+
+png(file ="chart/t1.png", width=1280,height=900,units="px") #大小及單位，這裡有需要再調
+t1 #要輸出的那個圖，原本在你Rstudio裡叫什麼
+dev.off()
+
+png(file ="chart/t2.png", width=1280,height=900,units="px") #大小及單位，這裡有需要再調
+t2 #要輸出的那個圖，原本在你Rstudio裡叫什麼
+dev.off()
+
+png(file ="chart/t3.png", width=1280,height=900,units="px") #大小及單位，這裡有需要再調
+t3 #要輸出的那個圖，原本在你Rstudio裡叫什麼
+dev.off()
+
+png(file ="chart/t4.png", width=1280,height=900,units="px") #大小及單位，這裡有需要再調
+t4 #要輸出的那個圖，原本在你Rstudio裡叫什麼
+dev.off()
