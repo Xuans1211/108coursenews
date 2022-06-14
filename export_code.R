e960d1b6bfa82933c4f4aaee11fec9158050caba
Xuans1211 <75928870+Xuans1211@users.noreply.github.com>
2022-06-14 17:32:34 +0800

    0614__code_final

--- export_code.R
+++ export_code.R
@@ -1,6 +1,6 @@
-png("chart/g3.png", #請自己打檔案名稱，"chart/"這段不要改
+png("chart/g_total.png", #請自己打檔案名稱，"chart/"這段不要改
     width=1280,height=900,units="px") #大小及單位，這裡有需要再調
-g3#要輸出的那個圖，原本在你Rstudio裡叫什麼
+g_total#要輸出的那個圖，原本在你Rstudio裡叫什麼
 dev.off()
 
 png(file ="chart/t1.png", width=1280,height=900,units="px") #大小及單位，這裡有需要再調
